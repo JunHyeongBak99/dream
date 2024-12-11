4bd175922f43d00d1ce5c97c1a78bc74e8a0e5c6
Bak Jun Hyeong <165124846+JunHyeongBak99@users.noreply.github.com>
2024-12-11 13:07:58 +0900

    Update dream.py

--- dream.py
+++ dream.py
@@ -4,6 +4,12 @@ import openai
 from datetime import datetime
 import re
 from langchain.chains import ConversationalRetrievalChain
+
+__import__('pysqlite3')
+import sys
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
